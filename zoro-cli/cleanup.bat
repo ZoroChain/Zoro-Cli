b899ed7a971dcf1bbdc527f105b6a55957a834a4
姚骏 <11@11.com>
2018-10-26 16:45:46 +0800

    更新清除数据库文件的批处理

--- zoro-cli/cleanup.bat
+++ zoro-cli/cleanup.bat
@@ -1,5 +1,5 @@
-rd bin\Debug\netcoreapp2.0\Chain_74746E41 /s /q
-rd bin\Debug\netcoreapp2.0\Index_74746E41 /s /q
+rd bin\Debug\netcoreapp2.0\Chain_0378734A /s /q
+rd bin\Debug\netcoreapp2.0\Index_0378734A /s /q
 rd bin\Debug\netcoreapp2.0\Logs /s /q
 rd bin\Debug\netcoreapp2.0\ApplicationLogs /s /q
 rd bin\Debug\netcoreapp2.0\AppChain /s /q
