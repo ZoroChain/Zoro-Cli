dffecd48f7c1c933577a10838c53e81c1cfc261d
Yao Jun <11@11.com>
2018-12-27 15:14:46 +0800

    更新bat，复制stresstesting项目的输出文件

--- copy-plugins.bat
+++ copy-plugins.bat
@@ -7,4 +7,7 @@ xcopy ..\Zoro-Plugins\SimplePolicy\SimplePolicy zoro-cli\bin\Debug\netcoreapp2.0
 xcopy ..\Zoro-Plugins\ApplicationLogs\bin\Debug\netstandard2.0\ApplicationLogs.dll zoro-cli\bin\Debug\netcoreapp2.0\Plugins\ /y /q
 xcopy ..\Zoro-Plugins\ApplicationLogs\ApplicationLogs zoro-cli\bin\Debug\netcoreapp2.0\Plugins\ApplicationLogs\ /y /q
 
+xcopy ..\Zoro-Plugins\StressTesting\bin\Debug\netstandard2.0\StressTesting.dll zoro-cli\bin\Debug\netcoreapp2.0\Plugins\ /y /q
+xcopy ..\Zoro-Plugins\StressTesting\StressTesting zoro-cli\bin\Debug\netcoreapp2.0\Plugins\StressTesting\ /y /q
+
 pause
