f2cc2a1e5892deb314f89571f2994cf8a786e72f
Michael Yao <michaely2008@sina.com>
2019-01-03 13:09:06 +0800

    update bat file

--- copy-plugins.bat
+++ copy-plugins.bat
@@ -10,4 +10,16 @@ xcopy ..\Zoro-Plugins\ApplicationLogs\ApplicationLogs zoro-cli\bin\Debug\netcore
 xcopy ..\Zoro-Plugins\StressTesting\bin\Debug\netstandard2.0\StressTesting.dll zoro-cli\bin\Debug\netcoreapp2.0\Plugins\ /y /q
 xcopy ..\Zoro-Plugins\StressTesting\StressTesting zoro-cli\bin\Debug\netcoreapp2.0\Plugins\StressTesting\ /y /q
 
+xcopy ..\Zoro-Plugins\RpcAgent\bin\Release\netstandard2.0\RpcAgent.dll zoro-cli\bin\Release\netcoreapp2.0\Plugins\ /y /q
+xcopy ..\Zoro-Plugins\RpcAgent\RpcAgent zoro-cli\bin\Release\netcoreapp2.0\Plugins\RpcAgent\ /y /q
+
+xcopy ..\Zoro-Plugins\SimplePolicy\bin\Release\netstandard2.0\SimplePolicy.dll zoro-cli\bin\Release\netcoreapp2.0\Plugins\ /y /q
+xcopy ..\Zoro-Plugins\SimplePolicy\SimplePolicy zoro-cli\bin\Release\netcoreapp2.0\Plugins\SimplePolicy\ /y /q
+
+xcopy ..\Zoro-Plugins\ApplicationLogs\bin\Release\netstandard2.0\ApplicationLogs.dll zoro-cli\bin\Release\netcoreapp2.0\Plugins\ /y /q
+xcopy ..\Zoro-Plugins\ApplicationLogs\ApplicationLogs zoro-cli\bin\Release\netcoreapp2.0\Plugins\ApplicationLogs\ /y /q
+
+xcopy ..\Zoro-Plugins\StressTesting\bin\Release\netstandard2.0\StressTesting.dll zoro-cli\bin\Release\netcoreapp2.0\Plugins\ /y /q
+xcopy ..\Zoro-Plugins\StressTesting\StressTesting zoro-cli\bin\Release\netcoreapp2.0\Plugins\StressTesting\ /y /q
+
 pause
